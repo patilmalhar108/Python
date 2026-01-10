b0907beddd745c092e4d01c01c202d05587ece40
patilmalhar108 <patilmalhar.a@gmail.com>
2026-01-10 06:39:02 -0600

    Add files via upload

---
+++ Activity2.py
@@ -0,0 +1,9 @@
+print("Python Operators")
+num1=24
+num2=7
+print("num1+num2=", num1+num2)
+print("num1-num2=", num1-num2)
+print("num1*num2=", num1*num2)
+print("num1/num2=", num1/num2)
+print("num1//num2=", num1//num2)
+print("remainder=", num1%num2)
