3e276c1c275fa0b3c97a83423183600b08eda961
patilmalhar108 <patilmalhar.a@gmail.com>
2026-01-15 06:46:57 -0600

    Add files via upload

---
+++ doublechecknumber.py
@@ -0,0 +1,9 @@
+print("Double Check Number")
+num=int(input("Enter a number: "))
+if num>50:
+    if num%2==0:
+        print("The number is even.")
+    else:
+        print("The number is odd.")
+else: 
+    print("The number is less than 50.")
