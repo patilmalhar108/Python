ca5d34b7d231ab8df1967bdde0dea2922972ecfc
patilmalhar108 <patilmalhar.a@gmail.com>
2026-01-22 06:30:18 -0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+x=int(input("Enter your first number: "))
+y=int(input("Enter your second number: "))
+print("Sum of 2 numbers: ", add(x,y))
+print("Difference of 2 numbers: ", sub(x,y))
+print("Product of 2 numbers: ", mul(x,y))
+print("Quotient of 2 numbers: ", div(x,y))
