05d51ad2d4856e6e2a452aeaab468319cb54de37
patilmalhar108 <patilmalhar.a@gmail.com>
2026-01-28 06:25:13 -0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,13 @@
+print("Operations on Lists")
+fruits = ["Apple","Banana","Orange","Strawberry","Blueberry"]
+print("Length of string: ",len(fruits))
+fruits.append("Mango")
+fruits.remove("Blueberry")
+fruits.sort()
+fruits.pop(4)
+fruits.reverse()
+fruits*2
+fruits = fruits[:3]
+#fruits.clear()
+fruits = fruits[-1]
+print(fruits)
