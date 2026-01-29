279238a3abe98e02377ac5817fd718fcf1510659
patilmalhar108 <patilmalhar.a@gmail.com>
2026-01-29 06:27:14 -0600

    Add files via upload

---
+++ union.py
@@ -0,0 +1,9 @@
+print("Set Union")
+set1 = {"Blue","Green","Red","Yellow","Orange"}
+set2 = {"Blue","Green","Purple","Yellow","Brown"}
+print("Original Sets")
+print(set1)
+print(set2)
+print("Sets After Union")
+set3 = set1.union(set2)
+print(set3)
