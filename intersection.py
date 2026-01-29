a93d3ba49ada38dcd7ab6f1be8d98c94f01c54b2
patilmalhar108 <patilmalhar.a@gmail.com>
2026-01-29 06:30:49 -0600

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,9 @@
+print("Set Intersections")
+set1 = {1,3,4,5,6,6,8}
+set2 = {1,2,3,5,5,7,8}
+print("Original Sets")
+print(set1)
+print(set2)
+print("After Intersection")
+set3 = set1.intersection(set2)
+print(set3)
