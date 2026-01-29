84ae5878f8be3e9360fe3a0a3d3b9f2d3de176ce
patilmalhar108 <patilmalhar.a@gmail.com>
2026-01-29 06:22:23 -0600

    Add files via upload

---
+++ set.py
@@ -0,0 +1,7 @@
+print("Operations on Set")
+myset = {1,1,2,3,4,4,5,8}
+print(myset)
+set1 = {2,4,6,7,8,9,9}
+set2 = {2,4,5,6,8,0}
+print(set1.difference(set2))
+print(set1.symmetric_difference(set2))
