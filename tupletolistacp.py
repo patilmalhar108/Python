5f567f8a635a16576a5e54561340e3b1605fb0b6
patilmalhar108 <patilmalhar.a@gmail.com>
2026-02-02 16:43:29 -0600

    Add files via upload

---
+++ tupletolistacp.py
@@ -0,0 +1,9 @@
+# Store personal data in a tuple
+personal_data = ("FirstName", "LastName", 16, 170.5, 60.2, "Favorite Subject")
+
+print("Tuple:", personal_data)
+
+# Convert the tuple to a list
+personal_data_list = list(personal_data)
+
+print("List:", personal_data_list)
