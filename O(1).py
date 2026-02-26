8fe560cc752aa9b72989fa834722a2cacbf1c62b
patilmalhar108 <patilmalhar.a@gmail.com>
2026-02-26 06:22:16 -0600

    Add files via upload

---
+++ O(1).py
@@ -0,0 +1,10 @@
+print("Program to show consetent time complexity by enterimng any input: ")
+def printnumber(n):
+    iteration = 0
+    print("The number entered by user is",n)
+    iteration = iteration+1
+    print("Total iteration done by code =", iteration)
+
+printnumber(10)
+printnumber(20)
+print("With any input, time taken by our code is not changing")
