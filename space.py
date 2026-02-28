0ca55e08dbce9c0e0762825336ddb226398f5e46
patilmalhar108 <patilmalhar.a@gmail.com>
2026-02-28 06:43:01 -0600

    Add files via upload

---
+++ space.py
@@ -0,0 +1,10 @@
+def sum(n):
+    return n*(n+1)//2
+print("Sum of first 'n' number is equal to: ",sum(8))
+def array_sum(a):
+    total = 0
+    for i in a:
+        total = total+1
+    return total
+a = [2,4,7,11,14,18]
+print("Sum of array is eqaul to:",array_sum(a))
